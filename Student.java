4735b25cb295c725c51e1ae50a51d770738cd178
Praveenkumarsaravanan76 <praveen161navy@gmail.com>
2026-03-21 13:47:35 +0530

    Pojoclass

---
+++ Student.java
@@ -0,0 +1,16 @@
+package com.example.POJO;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@AllArgsConstructor
+@Getter
+@Setter
+public class Student {
+
+    private int id;
+    private String firstName;
+    private String lastName;
+
+}
