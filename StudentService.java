e9eb86004b8651a8a76242088a0d0589d49045d0
Praveenkumarsaravanan76 <praveen161navy@gmail.com>
2026-03-29 11:22:44 +0530

    Service

---
+++ StudentService.java
@@ -0,0 +1,52 @@
+package com.example.h2connections.Service;
+
+import com.example.h2connections.Exception.StudentNotException;
+import com.example.h2connections.Repositary.StudentRepository;
+import com.example.h2connections.dao.Student1;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class StudentService {
+    @Autowired
+    private StudentRepository repo;
+    public Student1 SaveStudent(Student1 S){
+        return repo.save(S);
+    }
+    public List<Student1> getAllStudents(){
+
+        return repo.findAll();
+    }
+    public Student1 getStudentById(Integer id) {
+
+        return repo.findById(id)
+                .orElseThrow(() ->
+                        new StudentNotException("Student not found with id : " + id));
+    }
+
+    public Student1 getStudentByName(String name) {
+
+        return repo.findByName(name)
+                .orElseThrow(() ->
+                        new StudentNotException("Student not found with name : " + name));
+    }
+
+    public String deleteStudent(int id){
+        repo.deleteById(id);
+        return "Deleted successfully";
+    }
+    public Student1 updateStudent(int id,Student1 S){
+        Student1 existing=repo.findById(id).orElse(null);
+        if(existing!=null){
+            existing.setFirstName(S.getFirstName());
+            existing.setLastName(S.getLastName());
+            existing.setResult(S.getResult());
+            repo.save(existing);
+        }
+        return null;
+
+    }
+
+}
